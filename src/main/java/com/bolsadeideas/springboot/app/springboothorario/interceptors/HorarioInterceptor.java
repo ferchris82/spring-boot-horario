12aeb4e0020e3d63f047ec6ed08142942588f31c
ChrisferDev <ferchris82@hotmail.com>
2024-03-11 16:36:42 -0500

    Versión 1.1

--- src/main/java/com/bolsadeideas/springboot/app/springboothorario/interceptors/HorarioInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/app/springboothorario/interceptors/HorarioInterceptor.java
@@ -4,13 +4,14 @@ import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("horario")
 public class HorarioInterceptor implements HandlerInterceptor {
 
     @Value("${config.horario.apertura}")
@@ -46,7 +47,8 @@ public class HorarioInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
         String mensaje = (String) request.getAttribute("mensaje");
-        modelAndView.addObject("horario", mensaje);
+        if(modelAndView != null && handler instanceof HandlerMethod){
+            modelAndView.addObject("horario", mensaje);
+        }
     }
-
 }
